15747b355777ecf07a0f7a853e5acf2affce5973
rhmdziz <rahmadaziz288@gmail.com>
2024-12-07 22:12:37 +0700

    advance ui

---
+++ MyWebView/app/src/main/java/academy/bangkit/mywebview/MainActivity.kt
@@ -0,0 +1,40 @@
+package academy.bangkit.mywebview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG).show()
+                view.loadUrl("javascript:alert('Web Dicoding berhasil dimuat')")
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(view: WebView, url: String, message: String, result: android.webkit.JsResult): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                result.confirm()
+                return true
+            }
+        }
+        webView.loadUrl("https://www.dicoding.com")
+
+
+    }
+}
